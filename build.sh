2bd0cba8dcea02a597971460c822648426040b91
Thomas Bellembois <thomas.bellembois@uca.fr>
2018-11-15 11:25:57 +0100

    build.sh update

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ echo "building windows10 .exe"
 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ GOOS=windows GOARCH=amd64 go build -o dist/gochimitheque-win10.exe
 echo "building windows7 .exe"
 CGO_ENABLED=1 CC=i686-w64-mingw32-gcc CXX=i686-w64-mingw32-g++ GOOS=windows GOARCH=386 go build -o dist/gochimitheque-win7.exe
-echo "building windows7 binary"
+echo "building linux binary"
 go build -o dist/gochimitheque-linux
 
 rsync -av sample_* /tmp/
@@ -19,4 +19,4 @@ zip dist/gochimitheque-win10-demo.zip /tmp/sample_* /tmp/gochimitheque-win10.exe
 echo "building demo windows7 zip"
 zip dist/gochimitheque-win7-demo.zip /tmp/sample_* /tmp/gochimitheque-win7.exe
 echo "building demo linux zip"
-zip dist/gochimitheque-linux-demo.zip /tmp/sample_* /tmp/gochimitheque
+zip dist/gochimitheque-linux-demo.zip /tmp/sample_* /tmp/gochimitheque-linux
