f6248600ac7c828efa77841a6f267588f5f58ef2
Thomas Bellembois <thomas.bellembois@uca.fr>
2019-02-14 16:54:51 +0100

    cosmetics

--- locales/locales-en.go
+++ locales/locales-en.go
@@ -7,12 +7,47 @@ var LOCALES_EN = []byte(`
 
 [email_placeholder]
 	one = "enter your email"
+[submitlogin_text]
+	one = "enter"
 [password_placeholder]
 	one = "enter your password"
-[reset_password_title]
+[resetpassword_text]
 	one = "reset password"
+[resetpassword2_text]
+	one = "reset my password, I am not a robot"
 [logo_information1]
 	one = "Chimithèque logo designed by "
 [logo_information2]
 	one = "Do not use or copy without her permission."
+
+[menu_home]
+	one = "home"
+[menu_bookmark]
+	one = "my bookmarks"
+[menu_create_productcard]
+	one = "create product card"
+[menu_entity]
+	one = "entities"
+[menu_storelocation]
+	one = "store locations"
+[menu_people]
+	one = "people"
+[menu_password]
+	one = "reset my password"
+
+[clearsearch_text]
+	one = "clear search form"
+[search_text]
+	one = "search"
+[advancedsearch_text]
+	one = "advanced search"
+
+[switchproductview_text]
+	one = "switch to product view"
+[switchstorageview_text]
+	one = "switch to storage view"
+[export_text]
+	one = "export"
+[showhidedeleted_text]
+	one = "show deleted"
 `)
--- locales/locales-fr.go
+++ locales/locales-fr.go
@@ -7,12 +7,47 @@ var LOCALES_FR = []byte(`
 
 [email_placeholder]
 	one = "entrez votre email"
+[submitlogin_text]
+	one = "entrer"
 [password_placeholder]
 	one = "entrez votre mot de passe"
-[reset_password_title]
+[resetpassword_text]
 	one = "réinitialiser mon mot de passe"
+[resetpassword2_text]
+	one = "réinitialiser mon mot de passe, je ne suis pas un robot"
 [logo_information1]
 	one = "Logo Chimithèque réalisé par "
 [logo_information2]
 	one = "Ne pas utiliser ou copier sans sa permission."
+
+[menu_home]
+	one = "accueil"
+[menu_bookmark]
+	one = "mes favoris"
+[menu_create_productcard]
+	one = "créer fiche produit"
+[menu_entity]
+	one = "entités"
+[menu_storelocation]
+	one = "entrepôts"
+[menu_people]
+	one = "utilisateurs"
+[menu_password]
+	one = "réinitialiser mon mot de passe"
+
+[clearsearch_text]
+	one = "effacer le formulaire"
+[search_text]
+	one = "rechercher"
+[advancedsearch_text]
+	one = "recherche avancée"
+
+[switchproductview_text]
+	one = "vue par produits"
+[switchstorageview_text]
+	one = "vue par stockages"
+[export_text]
+	one = "exporter"
+[showhidedeleted_text]
+	one = "voir supprimés"
 `)
