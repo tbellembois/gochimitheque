da1d72f8db61c6cfb98820f70245b64bd91f7325
Thomas Bellembois <thomas.bellembois@uca.fr>
2019-03-13 16:22:07 +0100

    delete person improvements

--- models/model.go
+++ models/model.go
@@ -104,11 +104,11 @@ type Storage struct {
 
 // Borrowing represent a storage borrowing
 type Borrowing struct {
-	BorrowingID      sql.NullInt64  `db:"borrowing_id" json:"borrowing_id" schema:"borrowing_id"`
-	BorrowingComment sql.NullString `db:"borrowing_comment" json:"borrowing_comment" schema:"borrowing_comment"`
-	Person           `db:"person" json:"person" schema:"person"`
+	BorrowingID      sql.NullInt64                               `db:"borrowing_id" json:"borrowing_id" schema:"borrowing_id"`
+	BorrowingComment sql.NullString                              `db:"borrowing_comment" json:"borrowing_comment" schema:"borrowing_comment"`
+	Person           `db:"person" json:"person" schema:"person"` // logged person
 	Storage          `db:"storage" json:"storage" schema:"storage"`
-	Borrower         *Person `db:"borrower" json:"borrower" schema:"borrower"`
+	Borrower         *Person `db:"borrower" json:"borrower" schema:"borrower"` // logged person
 }
 
 // Permission represent who is able to do what on something
